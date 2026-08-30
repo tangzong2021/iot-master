d8b739aaa4d17cbb020e7fe4c56a5781a585a515
tangzong2021 <111994123+tangzong2021@users.noreply.github.com>
2026-08-30 13:40:49 +0800

    feat: history export button exports all product factors merged by time

--- pages/device_history.js
+++ pages/device_history.js
@@ -57,33 +57,80 @@ return {
       action: {
         type: 'script',
         script(data, index) {
-          this.request
-            .get('device/' + this.params.id + '/history/' + this.params.point, {
+          //导出当前时间范围内该设备的【全部因子】，按时间对齐合并为一个多列CSV
+          const doExport = (pid, points) => {
+            if (!points || !points.length) {
+              alert('产品未定义物模型点位，无法导出')
+              return
+            }
+            const query = {
               start: this.dayjs(this.toolbar.value.start).toISOString(),
               end: this.dayjs(this.toolbar.value.end).toISOString(),
               window: this.toolbar.value.window + this.toolbar.value.unit,
               method: this.toolbar.value.method
-            })
-            .subscribe(res => {
-              if (!res.data || !res.data.length) {
+            }
+            //逐点位拉取历史数据
+            Promise.all(points.map(p => {
+              return new Promise(resolve => {
+                this.request.get('device/' + this.params.id + '/history/' + p.name, query)
+                  .subscribe(res => resolve(res.data || []))
+              })
+            })).then(list => {
+              //按时间戳合并多因子行
+              const table = {}
+              list.forEach((records, idx) => {
+                const name = points[idx].name
+                records.map(r => {
+                  const row = table[r.time] || (table[r.time] = {})
+                  row[name] = r.value
+                })
+              })
+              const times = Object.keys(table).map(Number).sort((a, b) => a - b)
+              if (!times.length) {
                 alert('当前时间范围内没有数据')
                 return
               }
-              const rows = ['时间,' + this.params.point]
-              res.data.map(i => {
-                rows.push(this.dayjs(i.time).format('YYYY-MM-DD HH:mm:ss') + ',' + i.value)
+              const rows = ['时间,' + points.map(p => p.label + '(' + p.name + ')').join(',')]
+              times.map(t => {
+                rows.push(this.dayjs(t).format('YYYY-MM-DD HH:mm:ss') + ',' + points.map(p => {
+                  const v = table[t][p.name]
+                  return v === undefined ? '' : v
+                }).join(','))
               })
               //\ufeff BOM头，保证Excel打开中文不乱码
               const blob = new Blob(['\ufeff' + rows.join('\n')], {type: 'text/csv;charset=utf-8'})
               const url = URL.createObjectURL(blob)
               const a = document.createElement('a')
               a.href = url
-              a.download = this.params.id + '-' + this.params.point + this.dayjs().format('-YYYYMMDDHHmmss') + '.csv'
+              a.download = this.params.id + '-all-points' + this.dayjs().format('-YYYYMMDDHHmmss') + '.csv'
               document.body.appendChild(a)
               a.click()
               document.body.removeChild(a)
               URL.revokeObjectURL(url)
             })
+          }
+          //优先用页面参数里的产品ID，没有就查设备详情
+          if (this.params.product_id) {
+            const pid = this.params.product_id
+            this.request.get('product/' + pid + '/setting/model').subscribe(res => {
+              const points = []
+              ;(res.data && res.data.content ? res.data.content : []).map(p => (p.points || []).map(pt => points.push(pt)))
+              doExport(pid, points)
+            })
+          } else {
+            this.request.get('table/device/detail/' + this.params.id).subscribe(res => {
+              if (res.error || !res.data || !res.data.product_id) {
+                alert('找不到设备所属产品')
+                return
+              }
+              const pid = res.data.product_id
+              this.request.get('product/' + pid + '/setting/model').subscribe(r2 => {
+                const points = []
+                ;(r2.data && r2.data.content ? r2.data.content : []).map(p => (p.points || []).map(pt => points.push(pt)))
+                doExport(pid, points)
+              })
+            })
+          }
         }
       }
     },
